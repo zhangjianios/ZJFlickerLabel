20ce4ea07504b793e398fba42786deffb7238a3b
JianZ <343438569@qq.com>
2017-03-19 14:06:33 +0800

    ZJFlickerLabel
    ZJFlickerLabel

--- ZJFlickerLabel/ViewController.m
+++ ZJFlickerLabel/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "WSShiningLabel.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    WSShiningLabel *label1;
+}
 
 @end
 
@@ -17,8 +20,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor blackColor];
+    [self addLabel];
+    
 }
-
+-(void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        label1.isPlaying = false;
+//        label2.isPlaying = false;
+//        label3.isPlaying = false;
+//        label4.isPlaying = false;
+        [label1 startShimmer];
+//        [label2 startShimmer];
+//        [label3 startShimmer];
+//        [label4 startShimmer];
+    });
+}
+- (void)addLabel{
+    
+    label1 = [[WSShiningLabel alloc] init];
+    label1.frame = CGRectMake(50, 135, 350, 25);
+    label1.text = @"春来了，春天里，还记得那时候";
+    label1.textColor = [UIColor grayColor];
+    label1.font = [UIFont systemFontOfSize:20];
+    label1.shimmerColor = [UIColor orangeColor];   // 高亮颜色
+    [label1 startShimmer];                 // 开启闪烁
+    [self.view addSubview:label1];
+    
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
